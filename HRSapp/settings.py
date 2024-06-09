6f086f6eeb22d7754457ecaaf946c58a7a394763
unknown <drickcardiente1@gmail.com>
2024-06-09 10:25:54 -0700

    refres

--- HRSapp/settings.py
+++ HRSapp/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'HRSapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freedb_hrsystem',
+        'USER': 'freedb_admin_user',
+        'PASSWORD': 'DV!PNhJ7J5naAn4',
+        'HOST':'sql.freedb.tech',
+        'PORT':'3306',
     }
 }
 
